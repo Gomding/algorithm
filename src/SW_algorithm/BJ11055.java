3b6e1e6ed6a15c55851df25d63b6b0205688ab77
Gomding <backchida@gmail.com>
2020-06-05 16:44:49 +0900

    61. DP 백준 11055 - 가장 큰 증가 부분 수열

---
+++ src/SW_algorithm/BJ11055.java
@@ -0,0 +1,71 @@
+package SW_algorithm;
+
+/*
+
+DP 백준 11055 - 가장 큰 증가 부분 수열
+
+https://www.acmicpc.net/problem/11055
+
+문제
+수열 A가 주어졌을 때, 그 수열의 증가 부분 수열 중에서 합이 가장 큰 것을 구하는 프로그램을 작성하시오.
+
+예를 들어, 수열 A = {1, 100, 2, 50, 60, 3, 5, 6, 7, 8} 인 경우에 합이 가장 큰 증가 부분 수열은 A = {1, 100, 2, 50, 60, 3, 5, 6, 7, 8} 이고, 합은 113이다.
+
+입력
+첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000)이 주어진다.
+
+둘째 줄에는 수열 A를 이루고 있는 Ai가 주어진다. (1 ≤ Ai ≤ 1,000)
+
+출력
+첫째 줄에 수열 A의 합이 가장 큰 증가 부분 수열의 합을 출력한다.
+
+예제 입력 1
+10
+1 100 2 50 60 3 5 6 7 8
+
+예제 출력 1
+113
+
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ11055 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+
+        int[] nums = new int[N + 1];
+
+        int[] dp = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[1] = nums[1];
+
+        int max = dp[1];
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = nums[i];
+            for (int j = 1; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + nums[i]);
+                }
+            }
+            max = Math.max(dp[i], max);
+        }
+
+        System.out.println(max);
+
+    }
+}
