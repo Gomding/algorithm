c6650adf2e6e2162e1b6ba885cabbbc6e567d5a9
Gomding <backchida@gmail.com>
2020-10-10 12:49:41 +0900

    112. 프로그래머스 DFS 연습문제 1번

---
+++ src/Programmers/TargetNumber.java
@@ -0,0 +1,33 @@
+package Programmers;
+
+public class TargetNumber {
+
+    public static void main(String[] args) {
+
+        int[] input1 = {1, 1, 1, 1, 1};
+
+        System.out.println(solution(input1, 3));
+
+    }
+
+    public static int solution(int[] numbers, int target) {
+        int answer = 0;
+
+        answer = dfs(numbers, 0, 0, target);
+
+        return answer;
+    }
+
+    public static int dfs(int[] numbers, int node, int sum, int target) {
+        if(node == numbers.length) {
+            if (sum == target) {
+                return 1;
+            }
+            return 0;
+        }
+
+        return dfs(numbers, node + 1, sum + numbers[node], target) +
+                dfs(numbers, node + 1, sum - numbers[node], target);
+    }
+
+}
