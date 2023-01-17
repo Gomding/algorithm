f6710d72cd7061b7b2f293e29699fc8f90c0d143
Gomding <backchida@gmail.com>
2023-01-17 22:35:40 +0900

    [Programmers] refactor: 마라톤 문제 풀이

--- src/Programmers/Marathon.java
+++ src/Programmers/Marathon.java
@@ -3,6 +3,9 @@ package Programmers;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42576
+ */
 public class Marathon {
 
     public static void main(String[] args) {
@@ -15,7 +18,6 @@ public class Marathon {
     }
 
     public static String solution (String[] participant, String[] completion) {
-
         Map<String, Integer> nameMap = new HashMap<>();
 
         for (String name : participant) nameMap.put(name, nameMap.getOrDefault(name, 0) + 1);
@@ -27,7 +29,7 @@ public class Marathon {
             }
         }
 
-        return null;
+        throw new IllegalArgumentException("완주하지 못한 사람을 찾는데 실패했습니다.");
     }
 
 }
