1f27e6e05076a08321a1d8fa9566f8c354d2a44c
Gomding <backchida@gmail.com>
2023-01-17 23:49:07 +0900

    [Programmers] refactor: 전화번호부 문제 풀이

--- src/Programmers/PhoneBook.java
+++ src/Programmers/PhoneBook.java
@@ -1,5 +1,9 @@
 package Programmers;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
 
     public static void main(String[] args) {
@@ -10,6 +14,21 @@ public class PhoneBook {
 
         System.out.println(phoneBook.solution(input1));
         System.out.println(phoneBook.solution(input2));
+
+        System.out.println(phoneBook.solution2(input1));
+        System.out.println(phoneBook.solution2(input2));
+    }
+
+    public boolean solution2(String[] input) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String phoneNumber : input) {
+            for (int i =1; i < phoneNumber.length() + 1; i++) {
+                String currentWord = phoneNumber.substring(0, i);
+                map.put(currentWord, map.getOrDefault(currentWord, 0) + 1);
+            }
+        }
+        System.out.println(map);
+        return Arrays.stream(input).allMatch(it -> map.get(it) == 1);
     }
 
     public boolean solution(String[] input) {
